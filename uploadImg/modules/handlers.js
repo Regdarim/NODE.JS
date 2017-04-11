ee4a07a8fb77a8760020850cc9f1fd6e740064de
mac <mac@macbook-pro-mac.home>
2017-04-11 15:57:30 +0200

    wersja podstawowa

---
+++ uploadImg/modules/handlers.js
@@ -0,0 +1,38 @@
+var formidable = require('formidable');
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+}
+
+var fs = require('fs');
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
+    });
+}
+
+exports.error = function(req, res){
+	console.log('nie wiem co robić :(');
+	res.write("404 :(");
+	res.end();
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
+}
