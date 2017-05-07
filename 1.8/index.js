bde347888896a0dc3c5dfdcba5c51ec2e0784a77
mac <mac@macbook-pro-mac.home>
2017-05-07 16:33:55 +0200

    fix2

--- 1.8/index.js
+++ 1.8/index.js
@@ -7,7 +7,7 @@ server.on('request', function(request, response) {
             response.setHeader("Content-Type", "text/html; charset=utf-8");
             if (request.method == 'GET' && request.url === "/") {
                 fs.readFile('./index.html', function(err, data) {
-                    if (err) throw err;
+                    if (err)  console.log(err);
                     response.write(data);
                     response.end;
                 });
@@ -15,7 +15,7 @@ server.on('request', function(request, response) {
             } else {
 
                 fs.readFile('cat.jpg', function(err, data) {
-                        if (err) throw err; 
+                        if (err)  console.log(err); 
                         	response.statusCode = 404;
                             response.writeHead(200, {
                                 'Content-Type': 'image/jpeg'
